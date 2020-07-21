41367d6d0836b674ad71a2b1748e35686358f53f
xcgx <36234354+xcgx@users.noreply.github.com>
2020-07-21 22:56:26 +0800

    Update diy.sh

--- phicomm-k2p/diy.sh
+++ phicomm-k2p/diy.sh
@@ -1,3 +1,5 @@
 rm -Rf files/etc/profile.d/sysinfo.sh
 find target/linux/ramips/* -maxdepth 0 ! -path '*/patches-5.4' -exec rm -Rf '{}' \;
 svn co https://github.com/coolsnowwolf/lede/trunk/target/linux/ramips target/linux/ramips
+# Modify default IP
+sed -i 's/10.0.0.1/192.168.123.1/g' package/base-files/files/bin/config_generate
